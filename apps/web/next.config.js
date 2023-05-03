e3fb3bf49342989ae99d13fafd08c3fa263dea02
Ashary Vermaysha <asharyver13@gmail.com>
2023-05-03 07:59:30 +0700

    NextJS initial commit

---
+++ apps/web/next.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['ui'],
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../")
+  }
+}
+
+module.exports = nextConfig
